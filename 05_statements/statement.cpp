5df48827851730bd85cf00563a76cbd506faa07a
Toqeer Sheikh <iamtoqeer05@gmail.com>
2026-07-08 09:25:58 +0500

    05_statements

---
+++ 05_statements/statement.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// Expressions : are building blocks of statements.
+	// Statements: are building blocks of programming.
+	
+	// Expression is a most basic building block of a program.
+	
+	// A sequence of operators and operands that specifies a computation.
+	
+	// Computes a value from a number of operands.
+	
+	// Examples of expressions:
+	
+	cout<<3+4<<endl;
+	
+	cout<<4/2<<endl;
+	
+	cout<<(4>1)<<endl;
+	
+	cout<<(2==3)<<endl;
+	
+	
+    // Statement: A Complete line of code that perform some action.
+    
+    // Usually terminated by semi-colon.
+    
+    // Usually contain Expressions.
+    
+    // C++ has many types of statements:
+    
+    // For Example:
+    
+    // Expression, null, compound, selection, iteration, declaration, jump, try blocks, and others.
+    
+    // Example:
+    
+    int a{5};
+    int b{8};
+    
+    if(a<b) cout<<"B is greater."<<endl;
+    
+    
+    
+
+	
+	return 0;
+}
